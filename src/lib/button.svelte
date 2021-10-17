615c666906c0d05e20810fd3f223600445d25dfe
GeopJr <18014039+GeopJr@users.noreply.github.com>
2021-10-17 09:23:13 +0300

    fix: navigate correctly to external assets

--- src/lib/button.svelte
+++ src/lib/button.svelte
@@ -11,7 +11,7 @@
 		{href}
 		class={`btn ${circle ? 'circle' : ''}`}
 		target={external ? '_blank' : undefined}
-		rel={external ? 'noopener' : undefined}
+		rel={'external ' + external ? 'noopener' : ''}
 		on:click
 		aria-label={ariaLabel}
 	>
